927b345a63fdce9050a80d656325b3ce45ed7fb9
bogdanaKolic <108514766+bogdanaKolic@users.noreply.github.com>
2025-05-05 11:33:00 +0200

    Update build.sh - no seed

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -42,9 +42,10 @@ $CXX $CXXFLAGS -std=c++11 -I. \
      -o $OUT/libpng_read_fuzzer \
      -lFuzzingEngine .libs/libpng16.a -lz
 
-# add seed corpus.
-find $SRC/libpng -name "*.png" | grep -v crashers | \
-     xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
+# do not add seed corpus.
+#find $SRC/libpng -name "*.png" | grep -v crashers | \
+#     xargs zip $OUT/libpng_read_fuzzer_seed_corpus.zip
+echo no seed added
 
 cp $SRC/libpng/contrib/oss-fuzz/*.dict \
      $SRC/libpng/contrib/oss-fuzz/*.options $OUT/
